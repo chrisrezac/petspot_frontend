6c174c42c3af0da21a42885f5d330b23b2c76fe2
chris_rezac <rezacchristopher@gmail.com>
2021-03-22 17:21:57 -0400

    adds working router links to navbar in Bootstrap theme

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,136 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+      <div class="container">
+        <!-- Brand -->
+        <a class="navbar-brand" href="/index.html">
+          <img src="/assets/img/brand.svg" class="navbar-brand-img" alt="..." />
+        </a>
+
+        <!-- Toggler -->
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Collapse -->
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <!-- Navigation -->
+          <ul class="navbar-nav ml-auto">
+            <!-- Home Page -->
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                id="navbarLandings"
+                data-toggle="dropdown"
+                href="#"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <router-link to="/">Home</router-link>
+              </a>
+            </li>
+            <!-- Signup Page -->
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                id="navbarPages"
+                data-toggle="dropdown"
+                href="#"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <router-link v-if="!loggedIn()" to="/signup"
+                  >Sign Up</router-link
+                >
+              </a>
+              <!-- Login Page -->
+            </li>
+
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                id="navbarPages"
+                data-toggle="dropdown"
+                href="#"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <router-link v-if="!loggedIn()" to="/Login">Login</router-link>
+              </a>
+            </li>
+
+            <!-- Logout Page -->
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                id="navbarPages"
+                data-toggle="dropdown"
+                href="#"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <router-link v-if="loggedIn()" to="/logout"
+                  >Logout
+                </router-link>
+              </a>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                id="navbarPages"
+                data-toggle="dropdown"
+                href="#"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <router-link to="/pets">All Pets</router-link>
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                id="navbarPages"
+                data-toggle="dropdown"
+                href="#"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <router-link to="/posts">All Posts</router-link>
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                id="navbarPages"
+                data-toggle="dropdown"
+                href="#"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+              </a>
+            </li>
+          </ul>
+
+          <!-- Button -->
+          <a class="navbar-btn btn btn-sm btn-primary lift ml-auto">
+            <router-link
+              v-if="loggedIn()"
+              :to="`/users/${getUserId()}`"
+            ></router-link
+            >My Profile
+          </a>
+        </div>
+      </div>
+    </nav>
     <div id="nav">
       <router-link to="/">Home | </router-link>
       <router-link v-if="!loggedIn()" to="/signup">Sign Up | </router-link>
